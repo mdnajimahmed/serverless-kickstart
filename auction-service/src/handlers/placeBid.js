3bba7aa9dcdf01c98f8bad6bc75a2239d52c9a05
ivplay <najim.ju@gmail.com>
2022-01-09 13:05:52 +0600

    everything just works

--- auction-service/src/handlers/placeBid.js
+++ auction-service/src/handlers/placeBid.js
@@ -3,48 +3,53 @@
 const AWS = require('aws-sdk')
 const dynamodb = new AWS.DynamoDB.DocumentClient()
 const commonMiddleware = require('../lib/commonMiddleware').handler
+const createError = require('http-errors')
+
+// classic locking problem
+// use redis to lock the item
+// use sqs fifo queue to update auctions asynchrnously, use itemid as grp in fifo queue.
 
 const handler = async (event) => {
-  const {id} = event.pathParameters
-  const {amount} = event.body
-  // const params = {
-  //   TableName: process.env.AUCTION_TABLE_NAME,
-  //   Key: {id},
-  //   UpdateExpression: "set highestBid.amount = :amount",
-  //   ExpressionAttributeValues : {
-  //     ':amount':amount
-  //   },
-  //   ReturnValues: 'ALL_NEW'
-  // }
+  const { id } = event.pathParameters
+  const { amount } = event.body
   const params = {
     TableName: process.env.AUCTION_TABLE_NAME,
-    Key: {id},
+    Key: { id },
     UpdateExpression: "set highestBid = :highestBid",
-    ExpressionAttributeValues : {
-      ':highestBid':{
+    ExpressionAttributeValues: {
+      ':highestBid': {
         amount
-      }
+      },
+      ':amount':amount
+    },
+    ConditionExpression: "attribute_not_exists(#highestBid) or #highestBid.amount < :amount",
+    ExpressionAttributeNames: {
+      "#highestBid": "highestBid"
     },
     ReturnValues: 'ALL_NEW'
   }
 
   let updatedAuction = null
-  try{
+  try {
     const result = await dynamodb.update(params).promise();
     updatedAuction = result.Attributes
-    console.log("updatedAuction",updatedAuction)
-  }catch(e){
-    console.error(e)
+    console.log("updatedAuction", updatedAuction)
+  } catch (e) {
+    console.error("myerror = ",e)
+    console.error("debug => code = ",e.code,' eq = ',e.code == 'ConditionalCheckFailedException')
+    if (e.code == 'ConditionalCheckFailedException'){
+      throw new createError.BadRequest(`${amount} is not the highest bid`)
+    }
     throw new createError.InternalServerError(e)
   }
   return {
-    statusCode : 200,
+    statusCode: 200,
     body: JSON.stringify(updatedAuction)
   }
 };
 
-(()=> {
-  console.log("uuid",commonMiddleware)
-})()
+// (() => {
+//   console.log("uuid", commonMiddleware)
+// })()
 
 module.exports.handler = commonMiddleware(handler)
